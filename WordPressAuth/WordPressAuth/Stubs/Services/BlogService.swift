27e2a9dbeadee80f7ecae7459af731cad416ca05
Jorge Leandro Perez <jorge@lantean.co>
2018-02-26 13:39:21 -0300

    BlogService Stub: Nukes unused methods

--- WordPressAuth/WordPressAuth/Stubs/Services/BlogService.swift
+++ WordPressAuth/WordPressAuth/Stubs/Services/BlogService.swift
@@ -7,19 +7,11 @@ class BlogService {
 
     }
 
-    func blogCountForAllAccounts() -> Int {
-        return 0
-    }
-
     func fetchSiteInfo(forAddress: String, success: (SiteInfo) -> Void, failure: (Error) -> Void) {
         
     }
 
     func findBlog(withXmlrpc: String, andUsername: String) -> Blog? {
         return nil
-    }
-
-    func associateSyncedBlogs(toJetpackAccount: WPAccount, success: ()-> Void, failure: (Error) -> Void) {
-
     }
 }
